f80274b9b117124eda7cc031176dc13d50f9e2e0
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-01 06:25:26 +0100

    Update AirJump.cpp

--- Client/Syn/Module/Modules/Movement/AirJump.cpp
+++ Client/Syn/Module/Modules/Movement/AirJump.cpp
@@ -1,17 +1,10 @@
 #include "AirJump.h"
 
-class Actor {
-private:
-    char pad_0x0000[0x1D8]; //0x0000
-public:
-    bool onGround;
-};
-
 auto AirJump::onTick(void) -> void {
-    auto localPlayer = (Actor*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x045B7DE8, { 0x8, 0x18, 0xB8, 0x0 });
+    auto localPlayer = (uintptr_t*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x045B7DE8, { 0x8, 0x18, 0xB8, 0x0 });
 
     if(localPlayer == nullptr)
         return;
     
-    localPlayer->onGround = true;
+    *(bool*)((uintptr_t)(localPlayer) + 0x1D8) = true;
 };
