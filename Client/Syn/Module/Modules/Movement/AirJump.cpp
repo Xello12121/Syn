9e0fed5e0d1ec34335266979a1f6cb37e1fa829e
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-01 07:19:31 +0100

    Update AirJump.cpp

--- Client/Syn/Module/Modules/Movement/AirJump.cpp
+++ Client/Syn/Module/Modules/Movement/AirJump.cpp
@@ -8,4 +8,13 @@ auto AirJump::onTick(void) -> void {
         return;
     
     localPlayer->onGround = true;
+
+    if(!once) {
+        once = true;
+
+        auto myPos = *localPlayer->getPos();
+        auto msg = std::string("X: " + std::to_string(myPos.x) + ", Y: " + std::to_string(myPos.y) + ", Z: " + std::to_string(myPos.z));
+
+        Utils::debugLog(msg);
+    };
 };
