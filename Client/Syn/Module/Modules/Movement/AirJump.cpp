a5f4ea4d64f820b2178a6604ee586abd6f2995da
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-01 07:15:17 +0100

    Update AirJump.cpp

--- Client/Syn/Module/Modules/Movement/AirJump.cpp
+++ Client/Syn/Module/Modules/Movement/AirJump.cpp
@@ -1,4 +1,5 @@
 #include "AirJump.h"
+bool once = false;
 
 auto AirJump::onTick(void) -> void {
     auto localPlayer = MC::getLocalPlayer();
@@ -6,5 +7,5 @@ auto AirJump::onTick(void) -> void {
     if(localPlayer == nullptr)
         return;
     
-    *(bool*)((uintptr_t)(localPlayer) + 0x1D8) = true;
+    localPlayer->onGround = true;
 };
