7a885d4357792c0722952d73f89c2ddb6eb76187
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-02 04:09:26 +0100

    Create TestMod.h

---
+++ Client/Syn/Module/Modules/Misc/TestMod.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_SYN_MODULE_MODULES_MISC_TESTMOD
+#define CLIENT_SYN_MODULE_MODULES_MISC_TESTMOD
+
+#include "../../Module.h"
+
+class TestModule : public Module {
+public:
+    TestModule(Category* c) : Module(c, "TestModule") {
+        this->isEnabled = true;
+    };
+public:
+    auto onTick(void) -> void override;
+};
+
+#endif /* CLIENT_SYN_MODULE_MODULES_MISC_TESTMOD */
