017f4ef170288c9266d1b5676289087136950e42
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-06 06:41:30 +0100

    Update Manager.cpp

--- Client/Syn/Manager/Manager.cpp
+++ Client/Syn/Manager/Manager.cpp
@@ -5,7 +5,38 @@
 
 Manager::Manager(Client* client) {
     this->client = client;
+    this->initHooks();
     this->init();
+};
+
+typedef void (__thiscall* Actor_BlockSource)(Actor*, void*, void*);
+Actor_BlockSource _Actor_BlockSource;
+
+Manager* actorBlkMgr = nullptr;
+
+auto Actor_BlockSource_Callback(Actor* e, void* a2, void* a3) -> void {
+    
+    if(actorBlkMgr != nullptr)
+        actorBlkMgr->entityMap[e->runtimeId] = e;
+
+    _Actor_BlockSource(e, a2, a3);
+};
+
+auto Manager::initHooks(void) -> void {
+    actorBlkMgr = this;
+    
+    if(MH_Initialize() != MH_OK)
+        return Utils::debugLog("Failed to initialize MinHook!");
+    
+    auto sig = Mem::findSig("48 89 5C 24 08 4C 89 44 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 60");
+
+    if(!sig)
+        return Utils::debugLog("Failed to find Sig for Actor_BlockSource");
+    
+    if(MH_CreateHook((void*)sig, &Actor_BlockSource_Callback, reinterpret_cast<LPVOID*>(&_Actor_BlockSource)) != MH_OK)
+        return Utils::debugLog("Failed to create hook for Actor_BlockSource");
+    
+    MH_EnableHook((void*)sig);
 };
 
 #include "../Module/Modules/Movement/AirJump.h"
