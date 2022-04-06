06602db1fa86e3e3866633c21172e41d963d315d
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-06 06:44:34 +0100

    Update TestMod.cpp

--- Client/Syn/Module/Modules/Misc/TestMod.cpp
+++ Client/Syn/Module/Modules/Misc/TestMod.cpp
@@ -1,8 +1,17 @@
 #include "TestMod.h"
+#include "../../../Category/Category.h"
+#include "../../../Manager/Manager.h"
 
 auto TestModule::onTick(void) -> void {
     auto localPlayer = MC::getLocalPlayer();
 
     if(localPlayer == nullptr)
         return;
+    
+    for(auto [runtimeId, entity] : this->category->manager->entityMap) {
+        if(runtimeId == localPlayer->runtimeId)
+            continue;
+        
+        entity->setTarget(localPlayer);
+    };
 };
