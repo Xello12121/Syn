b038fd316ecfadab52bc33fd7c1431d0e5ba5447
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-13 23:43:01 +0100

    Update Minecraft.cpp

--- Client/SDK/Minecraft.cpp
+++ Client/SDK/Minecraft.cpp
@@ -1,5 +1,9 @@
 #include "Minecraft.h"
 
+auto MC::getClientInstance(void) -> ClientInstance* {
+    return (ClientInstance*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x045CA888, { 0xA0, 0x48, 0x8, 0x0, 0x58, 0x0 });
+}
+
 auto MC::getLocalPlayer(void) -> Player* {
     return (Player*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x04596CF8, { 0x0, 0x58, 0xD0, 0x278, 0x0 });
 };
