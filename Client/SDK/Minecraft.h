e9a06d90e3a3c0e71ec4c9af36a0cb065189424b
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-13 23:42:59 +0100

    Update Minecraft.h

--- Client/SDK/Minecraft.h
+++ Client/SDK/Minecraft.h
@@ -4,10 +4,12 @@
 #include "../Utils/Utils.h"
 #include "../Mem/Mem.h"
 
+#include "Classes/ClientInstance.h"
 #include "Classes/Player.h"
 
 class MC {
 public:
+    static auto getClientInstance(void) -> ClientInstance*;
     static auto getLocalPlayer(void) -> Player*;
 };
 
