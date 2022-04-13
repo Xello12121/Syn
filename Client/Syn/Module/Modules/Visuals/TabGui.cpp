b29c231cf4926c0cca5ad8971dd74a98e295067f
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-13 23:56:43 +0100

    Update TabGui.cpp

--- Client/Syn/Module/Modules/Visuals/TabGui.cpp
+++ Client/Syn/Module/Modules/Visuals/TabGui.cpp
@@ -2,11 +2,28 @@
 #include "../../../Category/Category.h"
 #include "../../../Manager/Manager.h"
 
+auto TabGui::updateAlpha(void) -> void {
+    auto instance = MC::getClientInstance();
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
+
+    auto modifier = 0.05f;
+
+    if(mcGame == nullptr || !mcGame->canUseKeys) {
+        if(alpha > 0.f)
+            alpha -= modifier;
+    } else {
+        if(alpha < 1.f)
+            alpha += modifier;
+    };
+};
+
 auto TabGui::onRender(Renderer* r) -> void {
     auto manager = this->category->manager;
     
     auto categories = manager->categories;
     auto modules = categories.at(selCatIndex)->modules;
+
+    this->updateAlpha();
 
     auto yOff = 0.f;
     auto rectWidth = 0;
@@ -25,8 +42,9 @@ auto TabGui::onRender(Renderer* r) -> void {
     };
 
     auto rectPos = Vec4<float>(4.f, 6.f, 16.f + rectWidth, yOff + 14.f);
-    r->fillRectangle(rectPos, Color(21, 21, 21, .4));
-    r->drawRectangle(rectPos, Color(255, 255, 255, .6), 2);
+    
+    r->fillRectangle(rectPos, Color(21, 21, 21, alpha));
+    r->drawRectangle(rectPos, Color(255, 255, 255, alpha), 2);
 
     auto I = 0;
     for(auto category : categories) {
@@ -40,10 +58,10 @@ auto TabGui::onRender(Renderer* r) -> void {
             if(selCatAnimModifier < (rectPos.z - 4.f))
                 selCatAnimModifier += animSpeedModifier;
 
-            r->fillRectangle(Vec4<float>(8.f, (I * height) + 9.f, selCatAnimModifier, ((I * height) + 9.f) + height), Color(3, 194, 252));
+            r->fillRectangle(Vec4<float>(8.f, (I * height) + 9.f, selCatAnimModifier, ((I * height) + 9.f) + height), Color(3, 194, 252, alpha));
         };
         
-        r->drawString(name, textSize, Vec2<float>(10.f, (I * height) + 10.f), Color(255, 255, 255));
+        r->drawString(name, textSize, Vec2<float>(10.f, (I * height) + 10.f), Color(255, 255, 255, alpha));
 
         I++;
     };
@@ -67,8 +85,8 @@ auto TabGui::onRender(Renderer* r) -> void {
 
     rectPos = Vec4<float>(rectPos.z, rectPos.y, rectPos.z + (16.f + rectWidth), rectPos.y + (yOff + 9.f));
     
-    r->fillRectangle(rectPos, Color(21, 21, 21, .4));
-    r->drawRectangle(rectPos, Color(255, 255, 255, .6), 2);
+    r->fillRectangle(rectPos, Color(21, 21, 21, alpha));
+    r->drawRectangle(rectPos, Color(255, 255, 255, alpha), 2);
 
     I = 0;
     for(auto mod : modules) {
@@ -82,15 +100,21 @@ auto TabGui::onRender(Renderer* r) -> void {
             if(selModAnimModifier < (rectPos.z - 6.f))
                 selModAnimModifier += animSpeedModifier;
 
-            r->fillRectangle(Vec4<float>(rectPos.x + 6.f, (I * height) + 10.f, selModAnimModifier, ((I * height) + 10.f) + height), Color(3, 194, 252));
+            r->fillRectangle(Vec4<float>(rectPos.x + 6.f, (I * height) + 10.f, selModAnimModifier, ((I * height) + 10.f) + height), Color(3, 194, 252, alpha));
         };
 
-        r->drawString(name, textSize, Vec2<float>(rectPos.x + 8.f, rectPos.y + (I * height) + 4.f), mod->isEnabled ? Color(25, 225, 150) : Color(255, 255, 255));
+        r->drawString(name, textSize, Vec2<float>(rectPos.x + 8.f, rectPos.y + (I * height) + 4.f), mod->isEnabled ? Color(25, 225, 150, alpha) : Color(255, 255, 255, alpha));
         I++;
     };
 };
 
 auto TabGui::onKey(uint64_t key, bool isDown, bool* cancel) -> void {
+    auto instance = MC::getClientInstance();
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
+
+    if(mcGame == nullptr || !mcGame->canUseKeys)
+        return;
+    
     if(!isDown || key < 0x25 || key > 0x28)
         return;
     
