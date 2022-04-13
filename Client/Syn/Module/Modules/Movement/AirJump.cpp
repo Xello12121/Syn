f5a0be47054c7080e8edff69c2d827a3585686cb
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-13 23:42:57 +0100

    Update AirJump.cpp

--- Client/Syn/Module/Modules/Movement/AirJump.cpp
+++ Client/Syn/Module/Modules/Movement/AirJump.cpp
@@ -1,10 +1,20 @@
 #include "AirJump.h"
 
 auto AirJump::onKey(uint64_t key, bool isDown, bool* cancel) -> void {
-    auto player = MC::getLocalPlayer();
+    auto instance = MC::getClientInstance();
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
     
-    if(!isDown || key != VK_SPACE || player == nullptr)
+    if(!isDown || key != VK_SPACE || mcGame == nullptr)
         return;
     
-    //
+    if(!mcGame->canUseKeys)
+        return;
+    
+    auto player = MC::getLocalPlayer();
+    
+    if(player == nullptr)
+        return;
+    
+    auto lerpTo = Vec3<float>(0.f, 0.45f, 0.f);
+    player->lerpMotion(&lerpTo);
 };
