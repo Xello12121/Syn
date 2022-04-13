48e9005277a7e486e7b4e318ee2299c5f0cc33ae
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-13 23:43:02 +0100

    Create MinecraftGame.h

---
+++ Client/SDK/Classes/MinecraftGame.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_SDK_CLASSES_MINECRAFTGAME
+#define CLIENT_SDK_CLASSES_MINECRAFTGAME
+
+class MinecraftGame {
+private:
+    char pad_0000[792]; //0x0000
+public:
+    bool canUseKeys;    //0x0318
+};
+
+#endif /* CLIENT_SDK_CLASSES_MINECRAFTGAME */
