d458f358b6fed108dcd0c34c649f8dc2f73aa9bd
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-24 22:51:22 +0100

    Update TabGui.cpp

--- Client/Syn/Module/Modules/Visuals/TabGui.cpp
+++ Client/Syn/Module/Modules/Visuals/TabGui.cpp
@@ -15,11 +15,8 @@ auto renderText(ImDrawList* draw, std::string text, float fontSize, ImVec2 pos, 
 auto TabGui::onRender(void) -> void {
 
     RenderUtils::setDrawList(ImGui::GetBackgroundDrawList());
-    
-    auto I = 0;
-    auto fontSize = 3.f;
 
-    for(auto category : this->category->manager->categories) {
+    /*for(auto category : this->category->manager->categories) {
         auto textRect = RenderUtils::getTextSize(category->name, 3.f);
         auto textPos = ImVec4(8.f, 8.f + I * (fontSize * 10), 12.f + textRect.x, (12.f + I * (fontSize * 10)) + textRect.y);
 
@@ -27,6 +24,15 @@ auto TabGui::onRender(void) -> void {
         RenderUtils::drawText(category->name, 3.f, ImVec2(textPos.x, textPos.y), ImColor(255, 255, 255));
 
         I++;
-    };
+    };*/
+
+    auto fontSize = 3.f;
+    auto text = std::string("Hello, World!");
+    
+    auto textRect = RenderUtils::getTextSize(text, fontSize);
+    auto textPos = ImVec4(8.f, 8.f + (fontSize * 10.f), 12.f + textRect.x, (12.f + (fontSize * 10.f) + textRect.y));
+
+    RenderUtils::fillRect(textPos, ImColor(21.f, 21.f, 21.f, .6f), 0.f);
+    RenderUtils::drawText(text, fontSize, ImVec2(textPos.x + 4.f, textPos.y), ImColor(255.f, 255.f, 255.f));
 
 };
