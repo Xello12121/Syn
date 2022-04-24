2f50ab349e0b9f1df9f6c4fca8c7df8f3d38d328
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-25 00:43:58 +0100

    Update TabGui.cpp

--- Client/Syn/Module/Modules/Visuals/TabGui.cpp
+++ Client/Syn/Module/Modules/Visuals/TabGui.cpp
@@ -16,23 +16,17 @@ auto TabGui::onRender(void) -> void {
 
     RenderUtils::setDrawList(ImGui::GetBackgroundDrawList());
 
-    /*for(auto category : this->category->manager->categories) {
-        auto textRect = RenderUtils::getTextSize(category->name, 3.f);
-        auto textPos = ImVec4(8.f, 8.f + I * (fontSize * 10), 12.f + textRect.x, (12.f + I * (fontSize * 10)) + textRect.y);
-
-        RenderUtils::fillRect(textPos, ImColor(0.2f, 0.2f, 0.2f), 0.f);
-        RenderUtils::drawText(category->name, 3.f, ImVec2(textPos.x, textPos.y), ImColor(255, 255, 255));
-
-        I++;
-    };*/
+    auto player = MC::getLocalPlayer();
+    auto instance = MC::getClientInstance();
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
 
     auto fontSize = 3.f;
-    auto text = std::string("Hello, World!");
+    auto text = std::string(mcGame != nullptr && mcGame->canUseKeys && player != nullptr ? player->getNameTag().c_str() : player != nullptr ? "Menu" : "Main Menu");
     
     auto textRect = RenderUtils::getTextSize(text, fontSize);
     auto textPos = ImVec4(8.f, 8.f + (fontSize * 10.f), 12.f + textRect.x, (12.f + (fontSize * 10.f) + textRect.y));
 
-    RenderUtils::fillRect(textPos, ImColor(21.f, 21.f, 21.f, .6f), 0.f);
-    RenderUtils::drawText(text, fontSize, ImVec2(textPos.x + 4.f, textPos.y), ImColor(255.f, 255.f, 255.f));
+    RenderUtils::fillRect(textPos, ImColor(25.f, 25.f, 25.f, .8f), 0.f);
+    RenderUtils::drawText(text, fontSize, ImVec2(textPos.x + 2.f, textPos.y), ImColor(255.f, 255.f, 255.f));
 
 };
