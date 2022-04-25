f58f567a35be80c3af3a339f817c6f141a786ee2
EchoHackCmd <EchoHackCmd@gmail.com>
2022-04-25 07:00:03 +0100

    Update TabGui.h

--- Client/Syn/Module/Modules/Visuals/TabGui.h
+++ Client/Syn/Module/Modules/Visuals/TabGui.h
@@ -11,6 +11,19 @@ public:
     };
 public:
     auto onRender(void) -> void override;
+    auto onKey(uint64_t, bool, bool*) -> void override;
+private:
+    float alpha = 0.f;
+private:
+    auto updateAlpha(void) -> void;
+    auto getScale(void) -> float;
+private:
+    bool selectedCat = true, selectedMod = false;
+    int selCatIndex = 0, selModIndex = 0;
+private:
+    float animModifier = 0.05f;
+    float selCatAnimOff = 0.f;
+    float selModAnimOff = 0.f;
 };
 
 #endif /* CLIENT_SYN_MODULE_MODULES_VISUALS_TABGUI */
