4b68e5b28925d4d205ad3870eb28a830abd93918
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-03 05:08:46 +0100

    Update TestMod.cpp

--- Client/Syn/Module/Modules/Misc/TestMod.cpp
+++ Client/Syn/Module/Modules/Misc/TestMod.cpp
@@ -3,18 +3,50 @@
 #include "../../../Manager/Manager.h"
 
 auto TestModule::onRender(void) -> void {
-    ImGui::Begin(std::string("Test Console").c_str());
+    ImGui::Begin(std::string("Test Module").c_str());
 
     if(!this->updatedWindowSize) {
         ImGui::SetWindowSize(ImVec2(600.f, 600.f));
         this->updatedWindowSize = true;
     };
 
-    if(ImGui::CollapsingHeader(std::string("Entity List").c_str())) {
-        for(auto [runtimeId, entity] : this->category->manager->entityMap) {
-            ImGui::Text(std::to_string(runtimeId).c_str());
+    auto player = MC::getLocalPlayer();
+    
+    if(ImGui::TreeNode(std::string("Entity Map").c_str())) {
+            
+        for(auto [runtimeId, entity] : category->manager->entityMap) {
+
+            ImGui::Spacing();
+            
+            if(ImGui::TreeNode(std::to_string(runtimeId).c_str())) {
+                
+                ImGui::Text(std::string("ID: " + std::to_string(runtimeId)).c_str());
+
+                if(ImGui::TreeNode(std::string("Options").c_str())) {
+                    
+                    if(ImGui::Button(std::string("Boost").c_str())) {
+                        
+                        auto lerpTo = Vec3(0.f, 1.f, 0.f);
+                        entity->lerpMotion(&lerpTo);
+
+                    };
+
+                    ImGui::TreePop();
+
+                };
+
+                ImGui::TreePop();
+
+            };
+
+            ImGui::Spacing();
+
         };
+
+        ImGui::TreePop();
+
     };
+
 
     ImGui::End();
 };
