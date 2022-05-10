ffa19568e5781ad369d84d6fc5e1987e1feb91ef
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-10 23:41:27 +0100

    Update Minecraft.cpp

--- Client/SDK/Minecraft.cpp
+++ Client/SDK/Minecraft.cpp
@@ -1,9 +1,13 @@
 #include "Minecraft.h"
+#include "Classes/Player.h"
 
 auto MC::getClientInstance(void) -> ClientInstance* {
     return (ClientInstance*)Mem::findMultiLvlPtr(0x05473C38, { 0x0, 0x48, 0x0 });
 };
 
 auto MC::getLocalPlayer(void) -> Player* {
-    return (Player*)Mem::findMultiLvlPtr(0x0549E7A8, { 0x18, 0x18, 0xB8, 0x0 });
+    auto player = (Player*)Mem::findMultiLvlPtr(0x0549E798, { 0x8, 0x20, 0xC8, 0x0 });
+    auto level = (player != nullptr ? player->getLevel() : nullptr);
+
+    return level != nullptr ? (Player*)level->getPrimaryLocalPlayer() : nullptr;
 };
