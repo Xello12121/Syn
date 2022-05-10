d08179b7e853dfd8928c5e4835ead2ab44187754
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-10 23:41:23 +0100

    Update Level.h

--- Client/SDK/Classes/Level.h
+++ Client/SDK/Classes/Level.h
@@ -13,6 +13,13 @@ public:
 
         return _FetchEntity(this, runtimeId, paramB);
     };
+public:
+    auto getPrimaryLocalPlayer(void) -> Actor* {
+        using GetPrimaryLocalPlayer = Actor* (__thiscall*)(Level*);
+        auto _GetPrimaryLocalPlayer = (GetPrimaryLocalPlayer)(this->VTable[59]);
+
+        return _GetPrimaryLocalPlayer(this);
+    };
 };
 
 #endif /* CLIENT_SDK_CLASSES_LEVEL */
