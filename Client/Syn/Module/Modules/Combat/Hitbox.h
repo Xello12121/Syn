2db589b35d3244e4d06fe2e2907e5b42439acfc3
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-11 01:52:06 +0100

    Update Hitbox.h

--- Client/Syn/Module/Modules/Combat/Hitbox.h
+++ Client/Syn/Module/Modules/Combat/Hitbox.h
@@ -9,10 +9,12 @@ public:
         //
     };
 public:
+    virtual auto onRenderOptions(void) -> void override;
     virtual auto onGameMode(GameMode*) -> void override;
     virtual auto onDisable(void) -> void override;
 private:
     std::map<uint8_t, Vec2<float>> types;
+    Vec2<float> size = Vec2<float>(2.f, 4.f);
 };
 
 #endif /* CLIENT_SYN_MODULE_MODULES_COMBAT_HITBOX */
