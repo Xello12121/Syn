9bf1d8b7708abf95a0160745a087a8f669dea449
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-14 00:43:49 +0100

    Create DirectionalPhase.h

---
+++ Client/Syn/Module/Modules/Player/DirectionalPhase.h
@@ -0,0 +1,20 @@
+#ifndef CLIENT_SYN_MODULE_MODULES_MOVEMENT_DIRECTIONALPHASE
+#define CLIENT_SYN_MODULE_MODULES_MOVEMENT_DIRECTIONALPHASE
+
+#include "../../Module.h"
+
+class DirectionalPhase : public Module {
+public:
+    DirectionalPhase(Category* c) : Module(c, "Directional Phase") {
+        this->key = 0x47; /* G */
+    };
+public:
+    auto onTick(void) -> void override;
+public:
+    auto onGameMode(GameMode*) -> void override;
+    auto onRenderOptions(void) -> void override;
+private:
+    float speed = 0.4f;
+};
+
+#endif /* CLIENT_SYN_MODULE_MODULES_MOVEMENT_DIRECTIONALPHASE */
